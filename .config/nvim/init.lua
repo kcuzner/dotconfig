6f74de3c7923e8d42885c75b3d52a2c9a1f41df1
Kevin Cuzner <kevin@kevincuzner.com>
2025-05-15 20:53:31 -0500

    Adding mini files for fuzzy search and opting into icons

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -6,6 +6,28 @@ vim.cmd("source ~/.vimrc")
 vim.g.python3_host_prog = "/usr/bin/python3"
 
 -- MiniFiles setup:
---   Mapped to <leader>f
+--   Mapped to Alt+f
 require('mini.files').setup()
-vim.keymap.set("n", "<leader>f", MiniFiles.open)
+vim.keymap.set("n", "<M-f>", MiniFiles.open)
+--   Open MiniFiles pointing at current file
+vim.keymap.set("n", "<M-F>", function ()
+  local _ = MiniFiles.close()
+    or MiniFiles.open(vim.api.nvim_buf_get_name(0), false)
+  vim.schedule(function()
+    MiniFiles.reveal_cwd()
+  end, 30)
+end)
+
+-- I like the mini icons
+require('mini.icons').setup()
+
+-- Mini pick setup. These blap the fzf configuration
+--
+-- I've found this to be considerably faster than fzf
+require('mini.pick').setup()
+vim.keymap.set("n", "<leader>s", function ()
+  MiniPick.builtin.files()
+end)
+vim.keymap.set("n", "<leader>S", function ()
+  MiniPick.builtin.cli({ command = {"rg", "--files", "--no-ignore", "--color=never"} });
+end)
